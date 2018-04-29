3a6af6dc095be6aba76c6876bab12bc95962f68e
fuadajip <fuadajipratomo@gmail.com>
2018-04-30 01:40:59 +0700

    graphql schema with dummy data

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const expressGraphQL = require("express-graphql");
 const schema = require("./schema");
 
 const app = express();
-app.use("/graphql", expressGraphQL({
+app.use(
+  "/graphql",
+  expressGraphQL({
     schema: schema,
-    graphiql: true,
-}))
+    graphiql: true
+  })
+);
 app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-})
+  console.log("Server is running on port 4000");
+});
--- schema.js
+++ schema.js
@@ -1,13 +1,55 @@
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
-    GraphQLList,
-    GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = require("graphql");
 
+// dummy data
+const customers = [
+  { id: "1", name: "Fuad", email: "fuad@gmail.com", age: 21 },
+  { id: "2", name: "Aji", email: "aji@gmail.com", age: 21 },
+  { id: "3", name: "Pratomo", email: "pratomo@gmail.com", age: 21 }
+];
 
+// customer type
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+// root query
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  }
+});
 module.exports = new GraphQLSchema({
-    
-})
+  query: RootQuery
+});
