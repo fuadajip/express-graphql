0fbb81cb2cc0e7bea1c7d9872c949e966eba2b61
fuadajip <fuadajipratomo@gmail.com>
2018-04-30 02:23:57 +0700

    server-json definitions and fetch from axios

--- schema.js
+++ schema.js
@@ -1,55 +1,62 @@
+const axios = require("axios");
 const {
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema,
-  GraphQLList,
-  GraphQLNonNull
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLNonNull
 } = require("graphql");
 
 // dummy data
-const customers = [
-  { id: "1", name: "Fuad", email: "fuad@gmail.com", age: 21 },
-  { id: "2", name: "Aji", email: "aji@gmail.com", age: 21 },
-  { id: "3", name: "Pratomo", email: "pratomo@gmail.com", age: 21 }
-];
+// const customers = [
+//   { id: "1", name: "Fuad", email: "fuad@gmail.com", age: 21 },
+//   { id: "2", name: "Aji", email: "aji@gmail.com", age: 21 },
+//   { id: "3", name: "Pratomo", email: "pratomo@gmail.com", age: 21 }
+// ];
 
 // customer type
 const CustomerType = new GraphQLObjectType({
-  name: "Customer",
-  fields: () => ({
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  })
+	name: "Customer",
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		email: { type: GraphQLString },
+		age: { type: GraphQLInt }
+	})
 });
 
 // root query
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    customer: {
-      type: CustomerType,
-      args: {
-        id: { type: GraphQLString }
-      },
-      resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id == args.id) {
-            return customers[i];
-          }
-        }
-      }
-    },
-    customers: {
-      type: new GraphQLList(CustomerType),
-      resolve(parentValue, args) {
-        return customers;
-      }
-    }
-  }
+	name: "RootQueryType",
+	fields: {
+		customer: {
+			type: CustomerType,
+			args: {
+				id: { type: GraphQLString }
+			},
+			resolve(parentValue, args) {
+				// for (let i = 0; i < customers.length; i++) {
+				//   if (customers[i].id == args.id) {
+				//     return customers[i];
+				//   }
+				// }
+
+				return axios
+					.get("http://localhost:3000/customers/" + args.id)
+					.then(res => res.data);
+			}
+		},
+		customers: {
+			type: new GraphQLList(CustomerType),
+			resolve(parentValue, args) {
+				return axios
+					.get("http://localhost:3000/customers")
+					.then(res => res.data);
+			}
+		}
+	}
 });
 module.exports = new GraphQLSchema({
-  query: RootQuery
+	query: RootQuery
 });
